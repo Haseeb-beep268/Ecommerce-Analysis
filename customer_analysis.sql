f032ff542610ab792bc5c5d121202f9b5e7d987d
Haseeb-beep268 <i237703@isb.nu.edu.pk>
2026-04-27 09:15:11 +0500

    Create customer_analysis.sql

---
+++ customer_analysis.sql
@@ -0,0 +1,10 @@
+SELECT 
+    c.customer_unique_id,
+    COUNT(DISTINCT o.order_id) AS total_orders,
+    ROUND(SUM(oi.price + oi.freight_value), 2) AS lifetime_value
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+GROUP BY c.customer_unique_id
+ORDER BY lifetime_value DESC
+LIMIT 10;
